950fa3d955138e2cdfb6450f74b7f674f1067902
bonzo <40222661+baramatsubonzo@users.noreply.github.com>
2021-07-20 08:57:59 +0900

    Initial commit

---
+++ src/Page1DetailA.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailA = () => {
+  return (
+    <div>
+      <h1>Page 1の詳細A</h1>
+    </div>
+  );
+};
---
+++ src/Page1DetailB.jsx
@@ -0,0 +1,7 @@
+export const Page1DetailB = () => {
+  return (
+    <div>
+      <h1>Page 1の詳細B</h1>
+    </div>
+  );
+};
